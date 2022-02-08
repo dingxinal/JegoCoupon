81711b5b92bc53e8a7621928db0cd7c5496aabd7
Dingxin Wu <dwu4@scu.edu>
2022-02-08 06:20:40 -0800

    refactor

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -6,54 +6,50 @@ import re
 from datetime import datetime
 import json
 
-async def main(mytimer: func.TimerRequest, sendGridMessage: func.Out[str]) -> None:
-    logging.info('Python HTTP trigger function processed a request.')
-    asession = AsyncHTMLSession()
+invalid_link = [354, 357, 157]
+url = 'https://cp.jegotrip.com.cn/partners/skuact/index.html?actName=act1&actId='
 
-    url = 'https://cp.jegotrip.com.cn/partners/skuact/index.html?actName=act1&actId='
-    email_value = ""
-    count = 0
+async def main(mytimer: func.TimerRequest, sendGridMessage: func.Out[str]) -> None:
     now = datetime.now()
     cur_date = now.isoweekday()
 
-    range1 = cur_date * 100
-    range2 = (cur_date + 1) * 100
-    email_value+= "遍历" + str(range1) + "至" + str(range2) + "\n"
+    asession = AsyncHTMLSession()
+
+    email_value = ""
+    count = 0
+
+    range1 = (cur_date - 1) * 100
+    range2 = cur_date * 100
+    email_value += "遍历" + str(range1) + "至" + str(range2) + "\n"
     for x in range(range1, range2):
-        if x == 354 or x == 357:
+        if x in invalid_link:
             continue
         r = await asession.get(url + str(x))
         await r.html.arender(timeout=20)
         html_file = r.html.html
-        text = ""
-        if "App" in html_file:
-            if "活动时间" in html_file: 
-                try:
-                    m = re.search("(\d+)年(\d+)月(\d+)日至(\d+)月(\d+)日", html_file)
-                    t = datetime(int(m.groups()[0]), int(m.groups()[3]), int(m.groups()[4]), 0, 0, 0, 0) if m else None
 
-                    m1 = re.search("至(\d+)年(\d+)月(\d+)日", html_file)
-                    t1 = datetime(int(m1.groups()[0]), int(m1.groups()[1]), int(m1.groups()[2]), 0, 0, 0, 0) if m1 else None
-                    if m and t and (now < t): 
-                        text = "Find a new link:" + url + str(x) + "\n"
-                        count+1
-                    elif m1 and t1 and (now < t1):
-                        text = "Find a new link:" + url + str(x) + "\n"
-                        count+1
-                    elif t1 is None and t is None:
-                        text = "A new link cannot find time:" + url + str(x) + "\n"
-                except Exception as e:
-                    logging.critical(e.__doc__)
-                    text = "Error happended in" + url + str(x) + ", the reason is" + e.__doc__
-            else:
-                text = "A new link with no time specifed:" + url + str(x) + "\n"
-            email_value+=text
+        if "App" not in html_file:
+            continue
+
+        try: 
+            time = detect_time(html_file)
+            if time and (now < time): 
+                email_value +=  "Find a new link:" + url + str(x) + "\n"
+                count += 1
+            elif time is None:
+                email_value +=  "A new link cannot find time:" + url + str(x) + "\n"
+        except Exception as e:
+            logging.critical(e.__doc__)
+            email_value +=  "Error happended in" + url + str(x) + ", the reason is" + e.__doc__
+
         r.close()
         asession.close()
     if count == 0:
-        email_value+="毫无发现 \n"
-        email_value+="------------------------------------------- \n"
-        email_value+="2/3 Update: https://cp.jegotrip.com.cn/partners/skuact/index.html?actName=act1&actId=354 \n  https://cp.jegotrip.com.cn/partners/skuact/index.html?actName=act1&actId=357"
+        email_value+="毫无发现"
+
+    email_value += """2/3 Update: https://cp.jegotrip.com.cn/partners/skuact/index.html?actName=act1&actId=354 \n  
+        https://cp.jegotrip.com.cn/partners/skuact/index.html?actName=act1&actId=357"""
+
     logging.info(email_value)
     message = {
             "personalizations": [{"to": [{"email": "wudingxin1996@gmail.com"}, {"email": "freshqiao@gmail.com"}]}],
@@ -62,3 +58,26 @@ async def main(mytimer: func.TimerRequest, sendGridMessage: func.Out[str]) -> No
         }
     
     sendGridMessage.set(json.dumps(message))
+
+def detect_time(html_file):
+    time = None
+    m = re.search("(\d+)年(\d+)月(\d+)日至(\d+)月(\d+)日", html_file)
+    if m:
+        time = datetime(int(m.groups()[0]), int(m.groups()[3]), int(m.groups()[4]), 0, 0, 0, 0)
+        return time
+
+    m1 = re.search("即日起至(\d+)年(\d+)月(\d+)日", html_file)
+    if m1:
+        time = datetime(int(m1.groups()[0]), int(m1.groups()[1]), int(m1.groups()[2]), 0, 0, 0, 0)
+        return time
+    
+    m2 = re.search("截止时间为(\d+)年(\d+)月(\d+)日", html_file)
+    if m2:
+        time = datetime(int(m2.groups()[0]), int(m2.groups()[1]), int(m2.groups()[2]), 0, 0, 0, 0)
+        return time
+    
+    m3 = re.search("截止日期为(\d+)年(\d+)月(\d+)日", html_file)
+    if m3:
+        time = datetime(int(m3.groups()[0]), int(m3.groups()[1]), int(m3.groups()[2]), 0, 0, 0, 0)
+        return time
+    return time
